e67edd2d21b75c77dc8a0cab5a3bea712a439d48
Arturo Rendon <arturo.rendon@unitec.edu>
2019-05-20 16:24:15 -0600

    Update mangoTree
    Additions for compatibility with visual representation.

--- LinkedList/mangoTree.cpp
+++ LinkedList/mangoTree.cpp
@@ -6,6 +6,18 @@ mangoTree::mangoTree()
 	this->first = nullptr;
 	this->last = nullptr;
 	this->counter = 0;
+	this->maxMangos = 0;
+	this->maxWeight = 0;
+}
+
+mangoTree::mangoTree(int _maxMango, double _maxWeight)
+{
+	this->first = nullptr;
+	this->last = nullptr;
+	this->counter = 0;
+	this->maxMangos = _maxMango;
+	this->maxWeight = _maxWeight;
+	this->weight = 0;
 }
 
 int mangoTree::getCounter()
@@ -25,64 +37,105 @@ void mangoTree::decreaseCounter()
 
 int mangoTree::addMango(mangoFruit* m)
 {
-	if (this->first == 0)
+	if (getCounter() < this->maxMangos && (this->weight+m->getWeight()) < this->maxWeight)
 	{
-		this->first = m;
-		this->last = m;
-	}
-	else
-	{
-		this->last->next = m;
-		m->prev = this->last;
-		this->last = m;
+		if (this->first == 0)
+		{
+			this->first = m;
+			this->last = m;
+		}
+		else
+		{
+			this->last->next = m;
+			m->prev = this->last;
+			this->last = m;
+		}
+		increaseCounter();
+		mangos.push_back(m);
+		this->weight += m->getWeight();
 	}
 
-	increaseCounter();
 	return 5;
 }
 
 int mangoTree::insertMango(mangoFruit* m, int pos)
 {
-	if (pos > (this->getCounter()+1))
-		return -1;
-
-	else if (pos < 0)
-		return -2;
-
-	else if(pos == (this->getCounter()+1))
+	if (getCounter() < this->maxMangos)
 	{
-		this->addMango(m);
-		return 0;
-	}
-	else if (pos == 1)
-	{
-		mangoFruit* tmp = this->first;
+		if (pos > (this->getCounter() + 1))
+			return -1;
 
-		m->prev = nullptr;
-		m->next = tmp;
-		tmp->prev = m;
-		this->first = m;
-		increaseCounter();
+		else if (pos < 0)
+			return -2;
 
-		return 1;
-	}
-	else if(pos <= this->getCounter())
-	{
-		mangoFruit* tmp = this->first;
-		int actualPos = 1;
-
-		while (actualPos < pos)
+		else if (pos == (this->getCounter() + 1))
 		{
-			actualPos++;
-			tmp = tmp->next;
+			this->addMango(m);
+			return 0;
+		}
+		else if (pos == 1)
+		{
+			if (this->weight+m->getWeight() < this->maxWeight)
+			{
+				mangoFruit* tmp = this->first;
+
+				m->prev = nullptr;
+				m->next = tmp;
+				tmp->prev = m;
+				this->first = m;
+				increaseCounter();
+				this->weight += m->getWeight();
+				mangos.clear();//Borramos todos los elementos para hacer el insert en el vector
+				mangoFruit* tmp = this->first;
+				for (int i = 0; i < getCounter(); i++)
+				{
+					if (i == pos - 1)
+						mangos.push_back(m);
+					else
+					{
+						mangos.push_back(tmp);
+						tmp = tmp->next;
+					}
+				}
+				return 1;
+			}
+			return 0;
+		}
+		else if (pos <= this->getCounter())
+		{
+			if (this->weight+m->getWeight() < this->maxWeight)
+			{
+				mangoFruit* tmp = this->first;
+				int actualPos = 1;
+
+				while (actualPos < pos)
+				{
+					actualPos++;
+					tmp = tmp->next;
+				}
+
+				m->prev = tmp->prev;
+				m->next = tmp;
+				m->prev->next = m;
+				tmp->prev = m;
+				increaseCounter();
+				mangoFruit* tmp = this->first;
+				mangos.clear();//Borramos todos los elementos para hacer el insert en el vector
+				for (int i = 0; i < getCounter(); i++)
+				{
+					if (i == pos - 1)
+						mangos.push_back(m);
+					else
+					{
+						mangos.push_back(tmp);
+						tmp = tmp->next;
+					}
+				}
+				return 2;
+			}
+			return 0;
 		}
 
-		m->prev = tmp->prev;
-		m->next = tmp;
-		m->prev->next = m;
-		tmp->prev = m;
-		increaseCounter();
-		return 2;
 	}
 
 	return -3;
@@ -123,7 +176,18 @@ int mangoTree::deleteMango(int pos)
 			tmp->next->prev = tmp->prev;
 		}
 
+		this->weight -= tmp->getWeight();
 		delete tmp;
+		mangos.clear();
+		mangoFruit* tmp = this->first;
+		for (int i = 0; i < getCounter(); i++)
+		{
+			if (i != pos-1)
+			{
+				mangos.push_back(tmp);
+				tmp = tmp->next;
+			}
+		}
 		decreaseCounter();
 		return 0;
 	}// Delete's if
@@ -141,3 +205,143 @@ void mangoTree::printMangoTree()
 	}
 
 }
+
+void mangoTree::mergeTrees(mangoTree* L2)
+{
+	this->maxMangos += L2->maxMangos;
+	this->maxWeight += L2->maxWeight;
+	this->counter += L2->counter;
+	this->weight += L2->weight;
+	mangoFruit* tmp = L2->first;
+
+	while (tmp->next != 0)
+	{
+		this->addMango(tmp);
+		mangos.push_back(tmp);
+		tmp = tmp->next;
+	}
+
+	L2->mangos.clear();
+
+}
+
+void mangoTree::menu(mangoTree &tree, mangoTree& tree2, bool &tener2arboles) {
+	bool* dosArboles = &tener2arboles;
+	mangoTree* arbol1 = &tree;
+	mangoTree* arbol2 = &tree2;
+	//mangoFruit le agregue = int x , int y, y (double,int,int)
+
+	//mangotree le agregue = mangos(es un vector);
+	while (true)
+	{
+		cout << "Que desea hacer?" << endl;
+		cout << "1. Agregar Mango" << endl;
+		cout << "2. Insertar Mango" << endl;
+		cout << "3. Eliminar Mango" << endl;
+		cout << "4. Activar segundo Arbol" << endl;
+		cout << "5. Desactivar segundo Arbol" << endl;
+		cout << "6. Unir Arboles" << endl;
+		int opcion;
+		cin >> opcion;
+
+
+		switch (opcion)
+		{
+		case 1:
+		{
+			if (*dosArboles)
+			{
+				cout << "En que arbol desea poner el nuevo mango?" << endl;
+				cout << "1. Arbol #1" << endl;
+				cout << "2. Arbol #2" << endl;
+				int arbol;
+				cin >> arbol;
+				switch (arbol)
+				{
+				case 1:
+				{
+					cout << "Ingrese el peso del mango";
+					int peso;
+					cin >> peso;
+					mangoFruit* mango = new mangoFruit();
+					mango->setWeight(peso);
+					mango->x = (rand() % 300) + 100;
+					mango->y = (rand() % 300) + 50;
+					arbol1->addMango(mango);
+					break;
+				}
+				case 2:
+				{
+					cout << "Ingrese el peso del mango";
+					int peso2;
+					cin >> peso2;
+					mangoFruit* mango2 = new mangoFruit();
+					mango2->setWeight(peso2);
+					mango2->x = (rand() % 300) + 550;
+					mango2->y = (rand() % 300) + 50;
+					arbol2->addMango(mango2);
+					break;
+				}
+
+				}
+
+			}
+			else {
+				cout << "Ingrese el peso del mango";
+				int peso;
+				cin >> peso;
+				mangoFruit* mango = new mangoFruit();
+				mango->setWeight(peso);
+				mango->x = (rand() % 300) + 100;
+				mango->y = (rand() % 300) + 50;
+				arbol1->addMango(mango);
+			}
+			break;
+		}
+		case 2:
+		{
+			//CODIGO DE INSERTAR MANGO
+			cout << "\nIngrese el peso del mango";
+			int peso;
+			cin >> peso;
+			cout << "\nIngrese la posicion deseada a donde insertar el mango: ";
+			int pos;
+			cin >> pos;
+			mangoFruit* mango = new mangoFruit();
+			mango->setWeight(peso);
+			mango->x = (rand() % 300) + 100;
+			mango->y = (rand() % 300) + 50;
+			arbol1->insertMango(mango, pos);
+			break;
+		}
+
+		case 3:
+		{
+			//CODIGO DE ELIMINAR MANGO
+			cout << "\nIngrese la posicion deseada a donde borrar el mango: ";
+			int pos;
+			cin >> pos;
+			arbol1->deleteMango(pos);
+			break;
+		}
+
+		case 4:
+			(*dosArboles) = true;
+			break;
+		case 5:
+			(*dosArboles) = false;
+			break;
+
+		case 6:
+		{
+			//CODIGO PARA UNIR MANGOS
+			arbol1->mergeTrees(arbol2);
+			break;
+		}
+		}
+
+
+
+	}
+}
+
--- LinkedList/mangoTree.h
+++ LinkedList/mangoTree.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "mangoFruit.h"
+#include <vector>
 
 using namespace std;
 
@@ -12,14 +13,21 @@ private:
 	void decreaseCounter();
 
 public:
+	vector<mangoFruit*> mangos;
 	mangoFruit* first;
 	mangoFruit* last;
+	int maxMangos;
+	double maxWeight;
+	double weight;
 
 	mangoTree();
+	mangoTree(int, double);
 	int addMango(mangoFruit* m);
 	int insertMango(mangoFruit* m, int position);
 	int deleteMango(int position);
 	void printMangoTree();
+	void mergeTrees(mangoTree*);
+	void menu(mangoTree&, mangoTree&, bool&);
 
 	int getCounter();
 
